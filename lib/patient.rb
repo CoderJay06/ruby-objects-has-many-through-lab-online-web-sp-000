5cc44aa7c193b917bc1af914b7325ec2635295b3
Jason J Cruz <jaycru06@yahoo.com>
2019-12-17 21:20:33 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -21,7 +21,7 @@ class Patient
   end 
   
   def new_appointment(doctor, date)
-    Appointment.new(date, self, Doctor.doctor)
+    Appointment.new(date, self, doctor)
   end 
   
   def appointments 
